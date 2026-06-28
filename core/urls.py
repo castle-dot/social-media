a30ef0ce34dc7dfc198b0aeb5cbed2ee81be18c8
castle-dot <caslte2wonderland@gmail.com>
2026-06-28 21:24:24 +0300

    Done

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from .views import index
 
+from django.urls import path
+from .views import index, signup, login, logout, settings
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('signup/', signup, name = 'signup'),
+    path('login/', login, name = 'login'),
+    path('logout/', logout, name = 'logout'),
+    path('settings/', settings, name = 'settings'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,70 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
+
+from .models import Profile
+
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+
+            # create profile
+            Profile.objects.create(
+                user=user
+            )
+
+
+            # automatically login
+            auth_login(request, user)
+
+
+            # send to settings
+            return redirect('settings')
+
+
+    else:
+
+        form = UserCreationForm()
+
+
+    return render(
+        request,
+        'signup.html',
+        {'form':form}
+    )
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
+    
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+
+def settings(request):
+    return render(request, 'setting.html')
